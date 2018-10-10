872fc7e46cdb3d63329d9ed6748ded0efb7d09e9
Rahul <rksethuram9@gmail.com>
2018-10-10 09:22:57 -0700

    Migrate with HST authorized

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,42 @@
 var EC = artifacts.require("./ECTools.sol");
 var LC = artifacts.require("./LedgerChannel.sol");
 const Vulnerable = artifacts.require("./VulnerableLedgerChannel.sol");
+const HumanStandardToken = artifacts.require(
+  "./lib/token/HumanStandardToken.sol"
+);
 
-module.exports = async function(deployer, network) {
+module.exports = async function(deployer, network, accounts) {
+  const authorizedTokens = [];
   deployer.deploy(EC);
-  deployer.link(EC, LC);
-  deployer.deploy(LC, []);
 
-  if (network !== "mainnet") {
+  if (network !== "mainnet" && network !== "rinkeby") {
     deployer.link(EC, Vulnerable);
     deployer.deploy(Vulnerable);
+
+    const supply = web3.utils.toBN(web3.utils.toWei("696969", "ether"));
+    await deployer.deploy(
+      HumanStandardToken,
+      supply,
+      "Test Token",
+      "18",
+      "TST"
+    );
+    const hst = await HumanStandardToken.deployed();
+    await Promise.all(
+      accounts.map(async (account, index) => {
+        if (index === 0) {
+          return;
+        }
+        return hst.transfer(
+          account,
+          supply.div(web3.utils.toBN(accounts.length))
+        );
+      })
+    );
+
+    authorizedTokens.push(hst.address);
   }
+
+  deployer.link(EC, LC);
+  deployer.deploy(LC, authorizedTokens);
 };
