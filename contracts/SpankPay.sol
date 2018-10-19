2f49b8aa22699000e22fc559680018a618b2a089
Ameen Soleimani <ameen.sol@gmail.com>
2018-10-18 21:06:28 -0700

    order pending onchain updates

--- contracts/SpankPay.sol
+++ contracts/SpankPay.sol
@@ -18,9 +18,9 @@ contract ChannelManager {
 
     // TODO figure out isDispute vs. status
     struct Channel {
-        uint256 weiBalances[3]; // [hub, user, total]
-        uint256 tokenBalances[3] // [hub, user, total]
-        uint256 txCount; // persisted onchain even when empty
+        uint256[3] weiBalances; // [hub, user, total]
+        uint256[3] tokenBalances // [hub, user, total]
+        uint256[2] txCount; // persisted onchain even when empty [global, onchain]
         bytes32 threadRoot;
         uint256 threadCount;
         address exitInitiator;
@@ -253,7 +253,7 @@ contract ChannelManager {
         uint256[2] pendingTokenDeposits, // [hub, user]
         uint256[2] pendingWeiWithdrawals, // [hub, user]
         uint256[2] pendingTokenWithdrawals, // [hub, user]
-        uint256 txCount, // persisted onchain even when empty
+        uint256[2] txCount, // persisted onchain even when empty
         bytes32 threadRoot,
         uint256 threadCount,
         uint256 timeout,
@@ -288,7 +288,8 @@ contract ChannelManager {
         require(hub == ECTools.recoverSigner(state, sigHub));
         require(user == ECTools.recoverSigner(state, sigUser));
 
-        require(txCount > channel.txCount, "txCount must be higher than the current txCount");
+        require(txCount[0] > channel.txCount[0], "global txCount must be higher than the current global txCount");
+        require(txCount[1] >= channel.txCount[1], "onchain txCount must be higher or equal to the current onchain txCount");
 
         // offchain wei/token balances do not exceed onchain total wei/token
         require(weiBalances[0].add(weiBalances[1]) <= channel.weiBalances[2], "wei must be conserved");
@@ -339,7 +340,7 @@ contract ChannelManager {
         uint256[2] pendingTokenDeposits, // [hub, user]
         uint256[2] pendingWeiWithdrawals, // [hub, user]
         uint256[2] pendingTokenWithdrawals, // [hub, user]
-        uint256 txCount, // persisted onchain even when empty
+        uint256[2] txCount, // persisted onchain even when empty
         bytes32 threadRoot,
         uint256 threadCount,
         uint256 timeout,
@@ -379,7 +380,8 @@ contract ChannelManager {
         require(hub == ECTools.recoverSigner(state, sigHub));
         require(user == ECTools.recoverSigner(state, sigUser));
 
-        require(txCount > channel.txCount, "txCount must be higher than the current txCount");
+        require(txCount[0] > channel.txCount[0], "global txCount must be higher than the current global txCount");
+        require(txCount[1] >= channel.txCount[1], "onchain txCount must be higher or equal to the current onchain txCount");
 
         // offchain wei/token balances do not exceed onchain total wei/token
         require(weiBalances[0].add(weiBalances[1]) <= channel.weiBalances[2], "wei must be conserved");
@@ -448,7 +450,7 @@ contract ChannelManager {
     // - yes, because I can exit with a previous state before the exchange operation is committed to chain
     // - hub / client need to deal with this edge case
 
-    // TODO - what if the most recent state has pending deposits / withdrawals that never got finalized?
+    // TODO - what if the most recent state has pending deposits / withdrawals that did or did not get finalized?
     // - how do we know they never got finalized?
     // - scenario1:
     //   1. user initiates deposit via userAuthorizedDeposit w/ no timeout
@@ -457,41 +459,152 @@ contract ChannelManager {
     //   4. if the hub doesn't acknlowedge the deposit, the user initiates a startExitWithUpdate
     //   5. the update still includes the pendingDeposit which finalized but wasn't acknowledged
     //
-    //   1 - Chain { weiBalances: [100, 100, 200] }
-    //   1 - Channel { weiBalances: [100, 100] }
+    //   1 - Chain { weiBalances: [100, 100, 200], txCount: [1, 1] }
+    //   1 - Channel { weiBalances: [100, 100, 200], txCount: [1, 1] }
 
-    //   2 - Chain { weiBalances: [100, 100, 200] }
-    //   2 - Channel { weiBalances: [100, 100], pendingDeposits: [0, 100] }
+    //   2 - Chain { weiBalances: [100, 100, 200], txCount: [1, 1] }
+    //   2 - Channel { weiBalances: [100, 100, 200], pendingDeposits: [0, 100], txCount: [2, 2] }
     //
     //   Deposit success
-    //   3 - Chain { weiBalances: [100, 200, 300] }
-    //   3 - Channel { weiBalances: [100, 100], pendingDeposits: [0, 100], txCount: 2 }
+    //   3 - Chain { weiBalances: [100, 200, 300], txCount: [2, 2] }
+    //   3 - Channel { weiBalances: [100, 100, 200], pendingDeposits: [0, 100], txCount: [2, 2] }
 
     //   Other offchain payment
-    //   4 - Chain { weiBalances: [100, 200, 300] }
-    //   4 - Channel { weiBalances: [110, 90], pendingDeposits: [0, 100], txCount: 3 }
+    //   4 - Chain { weiBalances: [100, 200, 300], [2, 2] }
+    //   4 - Channel { weiBalances: [110, 90, 200], pendingDeposits: [0, 100], txCount: [3, 2] }
 
     // - scenario2:
     //   1. user initiates deposit via userAuthorizedDeposit w/ no timeout
     //   2. user continues making payments / opening threads while it is being confirmed
     //   3. ***before*** the deposit tx confirms, the user initiates a startExitWithUpdate
     //   4. the update still includes the pendingDeposit which was never finalized
-    // - in s1, the user's weiBalance[1] will include the pendingDeposit
+
+    // - in s1, the user's weiBalance[1] will include the pendingDeposit (and weiBalance[2] - the total will reflect it too)
     // - in s2, the user's weiBalance[1] will **not** include the pendingDeposit
     // - this function needs to handle both cases (and generally all non time-sensitive deposit / withdrawal)
 
-    //   1 - Chain { weiBalances: [100, 100, 200] }
-    //   1 - Channel { weiBalances: [100, 100] }
+    //   1 - Chain { weiBalances: [100, 100, 200]  txCount: [1, 1]}
+    //   1 - Channel { weiBalances: [100, 100, 200], txCount: [1, 1] }
 
-    //   2 - Chain { weiBalances: [100, 100, 200] }
-    //   2 - Channel { weiBalances: [100, 100], pendingDeposits: [0, 100], txCount: 2 }
+    //   2 - Chain { weiBalances: [100, 100, 200]  txCount: [1, 1]}
+    //   2 - Channel { weiBalances: [100, 100, 200], pendingDeposits: [0, 100], txCount: [2, 2] }
 
     //   Other offchain payment
+    //   3 - Chain { weiBalances: [100, 100, 200]  txCount: [1, 1]}
+    //   3 - Channel { weiBalances: [110, 90, 200], pendingDeposits: [0, 100], txCount: [3, 2] }
+
+    // WILDCARD: what if the user never follows through with their pendingDeposit, and then the hub wants to withdraw?
+    // 1. hub closes their channel (byzantine) - this will work
+    // 2. friendly?
+    // - after this offchain state is committed to, what if the user *finally* submits the previous one?
+
+    //   2 - Chain { weiBalances: [100, 400, 200]  txCount: [1, 1]}
+    //   2 - Channel { weiBalances: [100, 350, 200], pendingWithdrawal: [0, 50], txCount: [2, 2], prevHash: 0 }
+
+    //   3 - Chain { weiBalances: [100, 400, 200]  txCount: [1, 1], hash: lkqjfkljalsdjf }
+    //  pay hub 10
+    //   3 - Channel { weiBalances: [110, 340, 200], pendingWithdrawal: [0, 50], txCount: [3, 2] }
+
+    //  withdraw more
+    //   3 - Channel { weiBalances: [110, 310, 200], pendingWithdrawal: [0, 80], txCount: [4, 3] , prevHash: 0 }
+
+    //   4.1 - user submits previous authorized onchain operation
+    //   - Chain { weiBalances: [100, 350, 50], txCount: [2, 2] }
+    //   - Channel { weiBalances: [110, 310, 200], pendingWithdrawal: [0, 80], txCount: [4, 3] }
+
+    // - Scenario3: deposit + withdrawal: SUCCESS
+    //   2 - Chain { weiBalances: [100, 100, 200] , [1, 1]}
+    //   2 - Channel { weiBalances: [100, 100, 200], pendingDeposits: [0, 100], pendingWithdrawals: [100, 0], txCount: [2, 2] }
+    //
+    //   Deposit success
+    //   3 - Chain { weiBalances: [0, 200, 200], [2, 2] }
+    //   2 - Channel { weiBalances: [100, 100, 200], pendingDeposits: [0, 100], pendingWithdrawals: [100, 0], txCount: [2, 2] }
+
+    //   Other offchain payment
+    //   4 - Chain { weiBalances: [0, 200, 200] }
+    //   4 - Channel { weiBalances: [110, 90, 200], pendingDeposits: [0, 100], pendingWithdrawals: [100, 0], txCount: [3, 2] }
+    //   CORRECT
+    //   4 - Channel { weiBalances: [10, 190, 200], pendingDeposits: [0, 100], pendingWithdrawals: [100, 0], txCount: 3 }
+
+    // - Scenario4: deposit + withdrawal: FAIL
+    //   2 - Chain { weiBalances: [100, 100, 200] }
+    //   2 - Channel { weiBalances: [100, 100, 200], pendingDeposits: [0, 100], pendingWithdrawals: [100, 0], txCount: 2 }
+    //
+    //   Deposit success
     //   3 - Chain { weiBalances: [100, 100, 200] }
-    //   3 - Channel { weiBalances: [110, 90], pendingDeposits: [0, 100], txCount: 3 }
+    //   2 - Channel { weiBalances: [100, 100, 200], pendingDeposits: [0, 100], pendingWithdrawals: [100, 0], txCount: 2 }
+
+    //   Other offchain payment
+    //   4 - Chain { weiBalances: [100, 100, 200] }
+    //   4 - Channel { weiBalances: [110, 90, 200], pendingDeposits: [0, 100], pendingWithdrawals: [100, 0], txCount: 3 }
+
+    // User has open channel
+    // weiBalances: [0, 100], txCount: [1, 1]
+    // weiBalances: [0, 100], pendingDeposits: [0, 100], txCount: [2, 2]
+    // 1. attempt 100 ETH deposit, fails (user doesn't have enough ETH)
+    // 2. user wallet says your 100 ETH deposit failed - try again? (YES) (NO)
+    //    YES - tries again...
+    //    NO - let's forget about it...
+    // weiBalances: [0, 100], txCount: [3, 1]
+    //
+    // Paths:
+    // 1. re-submit the old tx
+    // 2. try to deposit a different amount
+    // - weiBalances: [0, 100], pendingDeposits: [0, 1], txCount: [4, 2]
+    //   1. submit new tx success
+    //   2. old tx is submitted after new one is signed
+    //    - weiBalances: [0, 100], pendingDeposits: [0, 100], txCount: [2, 2]
+    //
+    // 3. make payments
+    // - problem, if other payments / state updates are made
+    // - weiBalances: [10, 90], txCount: [10, 1]
+    // 4. close out account
+
+    // 3 operations without a timer
+    // 1. hub withdraw
+    // 3. user withdraw - hub can inititate, no timer needed
+    // 4. hub deposit? - but what about depositing into open channels with performers?
+    // - didn't want to timeout for that...
+    // - hub can set timeout to 0, then it's fine...
+    // - if performer can submit this state, hub is liable, needs timer
+    // - In practice, this shouldn't matter, because the hub can just send the deposit themselves...
+    // - UPDATED RULE - timeouts only for liabilities OUTSIDE of the hub's control (and exchange)
+    // - we can add to liabilities anyways
+
+    // with timer
+    // 1. hub deposit into user balance (exchange)
+    // 2. hub deposit along with user deposit
+    // 3. user deposit
 
     // Problem - can't really rely on the offchain weiBalances values because some $$ could be in threads.
     // If we had offchain total value, that would probably be sufficient
+    // - no, because deposits/withdrawals can cancel each other out...
+    //
+    // Idea: use a different nonce/counter to track pending txs separate from txCount
+    // - pendingCounter?
+    // - batchNumber
+    // - chainCount, chainUpdates
+    // - chainTxCount
+
+    // Is it possible to invalidate a *onchain* txCount / pendingTx?
+    // - How does this work with timeout functions?
+    // - What if each onchain tx refers to each previous tx uniquely?
+
+    // For txCount[1] -> onChain ops -> the rule is to never ever add another pending op if the current one has not been confirmed onchain
+    // - it makes no difference if the pending state persists until the hub is ready / forced to exit the channel
+    // - when the hub exits the channel, they can send the state which will have the pending op still attached, but it will be ignored by the startExitWithUpdate fn
+    // - realistically, this only matter for user deposits, because that's the only time when the user HAS TO initiate.
+    //   - UX is tricky - users want to deposit ETH / tokens
+    //   - the client side can validate before this state update is actually signed
+    //     - user has enough ETH / tokens to execute
+    //     - enough tokens are approved
+    //     - what if tx fails or user never executes?
+    //     - as the user, now I try to deposit more money into the channel, I can't
+    //   - at what point will the hub exit your channel for you?
+
+    // FUCK IT
+    // use a timeout for user deposits
+    // hub will reject user state updates with pendingDeposit unless there is a timer
 
     // start exit with offchain state
     function startExitWithUpdate(
@@ -502,7 +615,7 @@ contract ChannelManager {
         uint256[2] pendingTokenDeposits, // [hub, user]
         uint256[2] pendingWeiWithdrawals, // [hub, user]
         uint256[2] pendingTokenWithdrawals, // [hub, user]
-        uint256 txCount, // persisted onchain even when empty
+        uint256[2] txCount, // [global, onchain] persisted onchain even when empty
         bytes32 threadRoot,
         uint256 threadCount,
         uint256 timeout,
@@ -538,28 +651,26 @@ contract ChannelManager {
         require(hub == ECTools.recoverSigner(state, sigHub));
         require(user == ECTools.recoverSigner(state, sigUser));
 
-        require(txCount > channel.txCount, "txCount must be higher than the current txCount");
+        require(txCount[0] > channel.txCount[0], "global txCount must be higher than the current global txCount");
+        require(txCount[1] >= channel.txCount[1], "onchain txCount must be higher or equal to the current onchain txCount");
 
         // offchain wei/token balances do not exceed onchain total wei/token
         require(weiBalances[0].add(weiBalances[1]) <= channel.weiBalances[2], "wei must be conserved");
         require(tokenBalances[0].add(tokenBalances[1]) <= channel.tokenBalances[2], "tokens must be conserved");
 
-        // normally, replace onchain w/ offchain + pending
+        // pending onchain txs have been executed - force update offchain state to reflect this
+        if (txCount[1] == channel.txCount[1]) {
+            weiBalances[0] = weiBalances[0].add(pendingWeiDeposits[0]).sub(pendingWeiWithdrawals[0]);
+            weiBalances[1] = weiBalances[1].add(pendingWeiDeposits[1]).sub(pendingWeiWithdrawals[1]);
+            tokenBalances[0] = tokenBalances[0].add(pendingTokenDeposits[0]).sub(pendingTokenWithdrawals[0]);
+            tokenBalances[1] = tokenBalances[1].add(pendingTokenDeposits[1]).sub(pendingTokenWithdrawals[1]);
+        }
 
-        // add pending withdrawals back in (only if they didn't work)
-
-
-        // update hub wei channel balance, account for deposit/withdrawal in reserves
-        channel.weiBalances[0] = weiBalances[0].add(pendingWeiDeposits[0]).sub(pendingWeiWithdrawals[0]);
-
-        // update user wei channel balance, account for deposit/withdrawal in reserves
-        channel.weiBalances[1] = weiBalances[1].add(pendingWeiDeposits[1]).sub(pendingWeiWithdrawals[1]);
-
-        // update hub token channel balance, account for deposit/withdrawal in reserves
-        channel.tokenBalances[0] = tokenBalances[0].add(pendingTokenDeposits[0]).sub(pendingTokenWithdrawals[0]);
-
-        // update user token channel balance, account for deposit/withdrawal in reserves
-        channel.tokenBalances[1] = tokenBalances[1].add(pendingTokenDeposits[1]).sub(pendingTokenWithdrawals[1]);
+        // set the channel wei/token balances
+        channel.weiBalances[0] = weiBalances[0];
+        channel.weiBalances[1] = weiBalances[1];
+        channel.tokenBalances[0] = tokenBalances[0];
+        channel.tokenBalances[1] = tokenBalances[1];
 
         // update state variables
         channel.txCount = txCount;
